6c3386d0ad936cf1a25e7e5437d849749c4225a1
Javier Florian <javier.florian@blend360.com>
2026-07-03 11:05:33 -0500

    feat(queries): se agrega query_06 con consulta de speed stats analysis

--- queries/query_06.sql
+++ queries/query_06.sql
@@ -6,8 +6,8 @@ join   stats s on ps.stat_id = s.id
 join   pokemon_types pt on ps.pokemon_id = pt.pokemon_id   
 join   "types" t on pt.type_id = t.id
 where  s.name_en = 'speed'
-and    ps.base_value >= 70
 and    pt.slot =1
 group by t.name_es 
 having count(distinct pt.pokemon_id) > 3
+ and   avg(ps.base_value) >= 70
 order by 2 desc
