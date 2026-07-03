bbbaa353cf284f8d8b2369fc6703433a5523692a
Javier Florian <javier.florian@blend360.com>
2026-07-03 11:09:50 -0500

    feat(queries): update query_06 to include descriptive aliases for selected fields

--- queries/query_06.sql
+++ queries/query_06.sql
@@ -1,6 +1,6 @@
 -- ⚠️  PLACEHOLDER: Este query aún no ha sido completado
 -- El entrenador asignado debe reemplazar este archivo con la consulta correcta.
-select t.name_es, count(distinct pt.pokemon_id), avg(ps.base_value )
+select t.name_es as tipo, count(distinct pt.pokemon_id) as cantidad_pokemon, avg(ps.base_value ) as promedio_velocidad
 from   pokemon_stats ps 
 join   stats s on ps.stat_id = s.id 
 join   pokemon_types pt on ps.pokemon_id = pt.pokemon_id   
