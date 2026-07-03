1d417032b07be0ca7866e58396ef98314bee9ec1
Lina Patricia Rubio Rodriguez <Lina.Rubio@blend360.com>
2026-07-03 11:03:01 -0500

    Query 05 - Pokedex con tipos y habilidades

--- queries/query_05.sql
+++ queries/query_05.sql
@@ -1,3 +1,11 @@
--- ⚠️  PLACEHOLDER: Este query aún no ha sido completado
--- El entrenador asignado debe reemplazar este archivo con la consulta correcta.
-SELECT * FROM query_pendiente_completar;
+SELECT 
+  p.name_es AS pokemon,
+  t.name_es AS tipo,
+  a.name_es AS habilidad
+FROM pokemon p
+INNER JOIN pokemon_types pt ON p.id = pt.pokemon_id
+INNER JOIN types t ON pt.type_id = t.id
+INNER JOIN pokemon_abilities pa ON p.id = pa.pokemon_id
+INNER JOIN abilities a ON pa.ability_id = a.id
+WHERE pa.is_hidden = false
+ORDER BY p.name_es, t.name_es, a.name_es;
