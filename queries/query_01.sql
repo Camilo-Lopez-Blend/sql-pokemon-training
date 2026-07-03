fbf0b6f01f45aae82bb3c0a360ab32f7f20d5686
Braider Urango <Braider.Urango@Blend360.com>
2026-07-03 10:58:29 -0500

    fix(query_01) ajuste el alias de las columnas en la query

--- queries/query_01.sql
+++ queries/query_01.sql
@@ -1,6 +1,6 @@
 -- ⚠️  PLACEHOLDER: Este query aún no ha sido completado
 -- El entrenador asignado debe reemplazar este archivo con la consulta correcta.
-select m.damage_class, AVG(m.power) as poder_promedio,
+select m.damage_class as clase, AVG(m.power) as promedio_poder,
 count(distinct m.id) as cantidad_movimientos, count(distinct pm.pokemon_id) as pokemon_distintos
 FROM moves m
 JOIN pokemon_moves pm
